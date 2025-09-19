c94139efdfd45236f059f06244a5003a867e494c
Nicolas Mesaritis <nmesaritis@ecommbx.com>
2025-09-19 08:07:52 +0300

    Initial commit from Specify template

---
+++ GameBuilder2d/GameBuilder2d.cpp
@@ -0,0 +1,12 @@
+﻿// GameBuilder2d.cpp : Defines the entry point for the application.
+//
+
+#include "GameBuilder2d.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
